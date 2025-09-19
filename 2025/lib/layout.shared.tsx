68805eae4140afe7aa3363352f8a3decff969e90
anakafeel <hashmisaim037@gmail.com>
2025-09-18 21:26:44 -0400

    feat(nav):add navbar logo

--- 2025/lib/layout.shared.tsx
+++ 2025/lib/layout.shared.tsx
@@ -1,4 +1,6 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import logo from '@/public/ARC_Logo-github.jpg';
+
 
 /**
  * Shared layout configurations
@@ -12,15 +14,8 @@ export function baseOptions(): BaseLayoutProps {
     nav: {
       title: (
         <>
-          <svg
-            width="24"
-            height="24"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-label="Logo"
-          >
-            <circle cx={12} cy={12} r={12} fill="currentColor" />
-          </svg>
-          My App
+    <img src={logo.src} alt="Logo" width={24} height={24} />
+          Arc Docs
         </>
       ),
     },
